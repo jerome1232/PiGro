5fd017a18140926c2a973f7defaf262a079e0244
Jeremy D. Jones <j.jones1232@gmail.com>
2021-02-23 21:19:07 -0700

    added documentation to header file

--- pi_talk_test/greenhouse.h
+++ pi_talk_test/greenhouse.h
@@ -5,13 +5,20 @@
 
 class Greenhouse {
     public:
+        // constructors
         Greenhouse();
-        Greenhouse(
-            int temp_low, int temp_high,
-            int humidity_low, int light_thresh);
+        Greenhouse(int , int ,int , int);
+        // initializes pins for input/output
         void begin();
+         /* runs actions based on sensor data turns on the heater if it's cold
+         * waters dry bays etc...
+         * must run run_sensor_check() at least once before running this
+         */
         void run_tasks();
+        /* Checks all sensors and stores them in private variables
+         */
         void run_sensor_check();
+        // getters
         bool get_light_status();
         bool get_heater_status();
         float get_temp();
@@ -19,7 +26,6 @@ class Greenhouse {
         int get_light_level();
         int get_soil_moisture_1();
         int get_soil_moisture_2();
-
     private:
         float _temp;
         float _humidity;
@@ -35,14 +41,17 @@ class Greenhouse {
         int _light_thresh;
         DHT _dht;
 
+        // Checks sensors and stores results
         float check_temp();
         float check_humidity();
         int check_light_level();
-        void water_pump_on_off(bool on_off);
         int check_soil_moisture_1();
         int check_soil_moisture_2();
-        void lights_on_off(bool on_off);
-        void heater_on_off(bool on_off);
+        // Turns device on/off based on bool sent
+        void water_pump_on_off(bool);
+        void lights_on_off(bool);
+        void heater_on_off(bool);
+        // handles the logic for turning on devices
         void operate_heat();
         void operate_light();
         void operate_water();
